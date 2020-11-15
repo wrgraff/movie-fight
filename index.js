ca2e1edbe0a3c1e941a0a28e87201ecd5c810ff6
Artur Trifonov <wrgraff@gmail.com>
2020-11-15 04:22:00 +0300

    add fetch data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'e247c6a0',
+            s: 'Dark Night'
+        }
+    });
+
+    console.log(response.data);
+};
+
+fetchData();
