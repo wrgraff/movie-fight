16a0e974a9791ab255eadc16ad99fc4265c5007b
Artur Trifonov <wrgraff@gmail.com>
2020-11-16 03:42:17 +0300

    add search with debounce

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const fetchData = async () => {
+const input = document.querySelector('input');
+input.addEventListener('input', debounce(onInput));
+
+function onInput(evt) {
+    fetchData(evt.target.value);
+};
+
+async function fetchData(term) {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'e247c6a0',
-            s: 'Dark Night'
+            s: term
         }
     });
 
     console.log(response.data);
 };
 
-fetchData();
+function debounce(func, delay = 500) {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        };
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
